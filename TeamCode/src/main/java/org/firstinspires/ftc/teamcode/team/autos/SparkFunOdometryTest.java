6dc4024d4fe9b8abeaaca3ebab1cc6d5d2c4458b
FTCTutus1Tuus <ftc19168@gmail.com>
2024-11-23 09:45:30 -0600

    Initial working SparkFun Odometry Tracking Optical Sensor (OTOS)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/SparkFunOdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/SparkFunOdometryTest.java
@@ -1,137 +1,77 @@
-package org.firstinspires.ftc.teamcode.team;
+//package org.firstinspires.ftc.teamcode.team.autos;
 
-import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+/*
+    SPDX-License-Identifier: MIT
+
+    Copyright (c) 2024 SparkFun Electronics
+*/
+//package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Config
-public class DarienOpMode extends LinearOpMode {
-
-    // HARDWARE COMPONENTS
-    public DcMotor omniMotor0; // left front
-    public DcMotor omniMotor1; // right front
-    public DcMotor omniMotor2; // left rear
-    public DcMotor omniMotor3; // right rear
-    public DcMotor verticalSlide;
-    public IMU imu;
-    public CRServo intakeSlide;
-    public Servo intakeWrist;
-    public CRServo intakeWheels;
-    //public ColorSensor intakeColorSensor;
-    //public TouchSensor intakeWristTouchSensor;
-    public Servo bucket;
-    public Servo cameraWrist;
-    public Servo specimenWrist;
-    public Servo specimenClaw;
-    public SparkFunOTOS myOtos;
-
-    // HARDWARE FIXED CONSTANTS
-    public static double encoderResolution = 537.7; //no change unless we change motors
-    public double wheelDiameter = 3.75; // inches
-    public double constMult = (wheelDiameter * (Math.PI));
-    public double inchesToEncoder = encoderResolution / constMult;
-    public static double rotationTolerance = 0.1;
-    public static double power = 0.3;
-    public static double powerIntakeWheelToPickupSample = 0.8;
-    public static double powerIntakeWheelToEjectSample = -0.3;
-    public static double powerIntakeSlideIn = -0.45;
-    public static double PI = 3.1415;
-
-    // HARDWARE TUNING CONSTANTS
-    public int encoderPos0, encoderPos1, encoderPos2, encoderPos3;
-    public int encoderPos;
-    public double regularDivBy = 1;
-    public double turboDivBy = 1;
-    public boolean turboBoost;
-
-    // Servo tuning constants
-
-    public static double bucketPickup = 0.95;
-    public static double bucketPlace = 0.75;
-
-    public static double specimenWristPlace = 0.13; // towards inside of robot - change name later?
-    public static double specimenWristPickup = 0.82;
-
-    public static double intakeWristGroundPosition = 0.6;
-    public static double intakeWristUpPosition = 0.025;
-
-    public static double specimenClawOpen = 0.82;
-    public static double specimenClawClosed = 0.88;
+/*
+ * This OpMode illustrates how to use the SparkFun Qwiic Optical Tracking Odometry Sensor (OTOS)
+ *
+ * The OpMode assumes that the sensor is configured with a name of "sensor_otos".
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ *
+ * See the sensor's product page: https://www.sparkfun.com/products/24904
+ */
+@TeleOp(name = "Sensor: SparkFun OTOS", group = "Sensor")
+//@Disabled
+public class SparkFunOdometryTest extends LinearOpMode {
+    // Create an instance of the sensor
+    SparkFunOTOS myOtos;
 
     @Override
     public void runOpMode() throws InterruptedException {
-    }
-
-    public void initControls() {
-        //isAuto: true=auto false=teleop
-        imu = hardwareMap.get(IMU.class, "imu");
-        imu.initialize(
-                new IMU.Parameters(
-                        new RevHubOrientationOnRobot(
-                                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
-                        )
-                )
-        );
-        imu.resetYaw();
-
+        // Get a reference to the sensor
         myOtos = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
+
+        // All the configuration for the OTOS is done in this helper method, check it out!
         configureOtos();
 
-        // INITIALIZE MOTORS
-        omniMotor0 = initializeMotor("omniMotor0");
-        omniMotor1 = initializeMotor("omniMotor1");
-        omniMotor2 = initializeMotor("omniMotor2");
-        omniMotor3 = initializeMotor("omniMotor3");
+        // Wait for the start button to be pressed
+        waitForStart();
 
-        omniMotor0.setDirection(DcMotor.Direction.REVERSE);
-        omniMotor1.setDirection(DcMotor.Direction.FORWARD);
-        omniMotor2.setDirection(DcMotor.Direction.FORWARD);
-        omniMotor3.setDirection(DcMotor.Direction.REVERSE);
+        // Loop until the OpMode ends
+        while (opModeIsActive()) {
+            // Get the latest position, which includes the x and y coordinates, plus the
+            // heading angle
+            SparkFunOTOS.Pose2D pos = myOtos.getPosition();
 
-        verticalSlide = initializeMotor("verticalSlide");
-        verticalSlide.setDirection(DcMotor.Direction.REVERSE);
-        verticalSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            // Reset the tracking if the user requests it
+            if (gamepad1.y) {
+                myOtos.resetTracking();
+            }
 
-        // INITIALIZE SERVOS
-        intakeSlide = hardwareMap.get(CRServo.class, "intakeSlide"); // CH port 3
-        intakeWrist = hardwareMap.get(Servo.class, "intakeWrist"); // CH port 1
-        intakeWheels = hardwareMap.get(CRServo.class, "intakeWheels"); // CH port 2
-        specimenClaw = hardwareMap.get(Servo.class, "specimenClaw");
-        specimenWrist = hardwareMap.get(Servo.class, "specimenWrist");
-        bucket = hardwareMap.get(Servo.class, "bucket");
+            // Re-calibrate the IMU if the user requests it
+            if (gamepad1.x) {
+                myOtos.calibrateImu();
+            }
 
-    }
+            // Inform user of available controls
+            telemetry.addLine("Press Y (triangle) on Gamepad to reset tracking");
+            telemetry.addLine("Press X (square) on Gamepad to calibrate the IMU");
+            telemetry.addLine();
 
-    public double getVoltage() {
-        return (hardwareMap.voltageSensor.iterator().next().getVoltage());
-    }
+            // Log the position to the telemetry
+            telemetry.addData("X coordinate", pos.x);
+            telemetry.addData("Y coordinate", pos.y);
+            telemetry.addData("Heading angle", pos.h);
 
-    public void print(String Name, Object message) {
-        //saves a line for quick debug messages
-        telemetry.addData(Name, message);
-        telemetry.update();
-    }
-
-    public double relativePower(double intended_power) {
-        //makes sure the power going to the motors is constant over battery life
-        return (13 * intended_power) / getVoltage();
-    }
-
-    public DcMotor initializeMotor(String name) {
-         /*This is just a handy dandy function which saves a few lines and looks cool,
-         it initializes the motor and it also initializers the motor power logs for this motor*/
-        DcMotor motor = hardwareMap.get(DcMotor.class, name);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        return motor;
+            // Update the telemetry on the driver station
+            telemetry.update();
+        }
     }
 
     private void configureOtos() {
@@ -215,5 +155,4 @@ public class DarienOpMode extends LinearOpMode {
         telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
         telemetry.update();
     }
-
 }
