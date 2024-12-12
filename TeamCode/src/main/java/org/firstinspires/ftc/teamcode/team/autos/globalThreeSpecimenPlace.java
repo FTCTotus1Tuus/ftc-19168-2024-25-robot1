a2ef0191a87d0aace045c53f1349300d94947071
FTCTutus1Tuus <ftc19168@gmail.com>
2024-12-11 19:55:07 -0600

    finalized specimen side for meeeeeet 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/globalThreeSpecimenPlace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/globalThreeSpecimenPlace.java
@@ -11,10 +11,11 @@ public class globalThreeSpecimenPlace extends DarienOpModeAuto {
         initControls();
         waitForStart();
 
+        // Specimen 1
+        moveToPosition(-2, 31, 0.4);
         setSpecimenClaw("closed");
         setSpecimenWrist("place");
         setVerticalSlide("high chamber below", verticalSlidePower);
-        moveToPosition(-2, 31, 0.4);
         waitForMotors(2);
         waitForArm();
 
@@ -24,24 +25,59 @@ public class globalThreeSpecimenPlace extends DarienOpModeAuto {
         setSpecimenClaw("open");
         setVerticalSlide("low", verticalSlidePower);
 
-        moveToPosition(-1, 25, normalPower);
-        waitForMotors(2);
-        moveToPosition(36, 25, 0.4);
+        // Push one floor sample into observation zone, which will become specimen #3.
+        moveToPosition(-2, 25, normalPower);
+        waitForMotors(0.5);
+        moveToPosition(30, 25, 0.4);
         waitForMotors();
-        moveToPosition(36, -3, 0.4);
-        waitForMotors(3);
+        moveToPosition(30, 47, 0.4);
+        waitForMotors(1.5);
+        moveToPosition(40, 47, 0.4);
+        waitForMotors(1);
+        moveToPosition(40, 6, 0.7);
+        waitForMotors(2);
+        moveToPosition(32, 4, 0.4);
+        waitForMotors(1.5);
+        moveToPosition(32, -3, 0.4);
+        waitForMotors(1);
 
-        //pickup specimen from wall
+        // Specimen 2: Pick up specimen from wall
         setSpecimenClaw("closed");
-        sleep(250);
+        sleep(150);
         setSpecimenWrist("place");
 
-        moveToPosition(36, 15, 0.5);
+        // Go to the sub to score the specimen
         setVerticalSlide("high chamber below", verticalSlidePower / 2);
+        moveToPosition(-5, 16, 0.8);
+        waitForMotors(2);
+        moveToPosition(-5, 39, normalPower);
+        waitForMotors(1);
+        waitForArm();
+        setVerticalSlide("high chamber place", verticalSlidePower);
+        waitForArm();
+        setSpecimenWrist("pickup");
+        setSpecimenClaw("open");
+        setVerticalSlide("low", verticalSlidePower);
+
+        // Specimen 3
+        // Go to observation zone to pick up specimen from wall
+        moveToPosition(-1, 25, normalPower);
+        waitForMotors(0.5);
+        moveToPosition(36, 25, 0.4);
         waitForMotors();
-        moveToPosition(-5, 16, 0.6);
-        waitForMotors(3);
-        moveToPosition(-5, 37, normalPower);
+        moveToPosition(36, -3, 0.5);
+        waitForMotors(1.5);
+
+        // Specimen 3: Pickup specimen from wall
+        setSpecimenClaw("closed");
+        sleep(150);
+        setSpecimenWrist("place");
+        setVerticalSlide("high chamber below", verticalSlidePower / 2);
+
+
+        moveToPosition(-8, 16, 0.8);
+        waitForMotors(2);
+        moveToPosition(-8, 40, 0.4);
         waitForMotors(1);
         waitForArm();
         setVerticalSlide("high chamber place", verticalSlidePower);
@@ -51,34 +87,27 @@ public class globalThreeSpecimenPlace extends DarienOpModeAuto {
 
         setVerticalSlide("low", verticalSlidePower);
 
-        moveToPosition(-1, 25, normalPower);
-        waitForMotors(2);
-        moveToPosition(36, 25, 0.4);
-        waitForMotors();
-        moveToPosition(36, -3, 0.4);
-        waitForMotors(3);
+        // PARK: Go to observation zone.
+        moveToPosition(40, 5, 1);
 
-        //pickup specimen from wall
-        setSpecimenClaw("closed");
-        sleep(250);
-        setSpecimenWrist("place");
+        double errorX;
+        double errorY;
+        double errorXp;
+        double errorYp;
+        
+        while (opModeIsActive()) {
+            errorX = 40 - getXPos();
+            errorY = 5 - getYPos();
+            errorXp = (errorX * Math.cos(Math.toRadians(getRawHeading()))) + errorY * Math.sin(Math.toRadians(getRawHeading()));
+            errorYp = (-errorX * Math.sin(Math.toRadians(getRawHeading()))) + errorY * Math.cos(Math.toRadians(getRawHeading()));
 
-        moveToPosition(36, 15, 0.5);
-        setVerticalSlide("high chamber below", verticalSlidePower / 2);
-        waitForMotors();
-        moveToPosition(-9, 15, 0.6);
-        waitForMotors(3);
-        moveToPosition(-9, 37, normalPower);
-        waitForMotors(1);
-        waitForArm();
-        setVerticalSlide("high chamber place", verticalSlidePower);
-        waitForArm();
-        setSpecimenWrist("pickup");
-        setSpecimenClaw("open");
-
-        setVerticalSlide("low", verticalSlidePower);
-
-        moveToPosition(40, 5, 0.8);
-
+            if (getHypotenuse(errorXp, errorYp) > 5) {
+                setPower(currentMovementPower, errorXp, errorYp); // add pid?
+            } else {
+                setPower(currentMovementPower / 2, errorXp, errorYp); // add pid?
+            }
+        }
     }
+
 }
+
