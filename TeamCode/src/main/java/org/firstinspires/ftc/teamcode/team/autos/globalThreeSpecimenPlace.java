6a565704455a7130bc3d541d1b25d8fc63e8ec1a
FTCTutus1Tuus <ftc19168@gmail.com>
2025-01-11 14:30:17 -0600

    disable 2 auto programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/globalThreeSpecimenPlace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/autos/globalThreeSpecimenPlace.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.team.autos;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.team.DarienOpModeAuto;
 
+@Disabled
 @Autonomous
 public class globalThreeSpecimenPlace extends DarienOpModeAuto {
     @Override
